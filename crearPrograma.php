aeb5de61b05233137400f9d764ad36abe2229f77
CECasasola <cecm2893@gmail.com>
2017-10-30 07:39:39 -0600

    parte inicial de crearPrograma.php

---
+++ crearPrograma.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+
+
+</head>
+<body>
+    <h1 align="center">Aperturar programa</h1>
+
+    <div class="alert alert-warning">
+      <strong>Precaución.</strong> Antes de aperturar un nuevo programa, debe cerrar el existente.
+      Hágalo solamente si ya no necesitará modificar la información del programa actual. Si está
+      segur@ de hacerlo, proceda seleccionando la opción Cerrar Programa.
+    </div>
+    <br>
+
+
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/dataTables.bootstrap.min.js"></script>
+</body>
+</html>
