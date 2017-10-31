36b27c7a399794a9b0a10a0d6f8a37d1a9490ab7
CECasasola <cecm2893@gmail.com>
2017-10-30 21:55:55 -0600

    terminada funcion de aperturar Programa

--- crearPrograma.php
+++ crearPrograma.php
@@ -32,9 +32,7 @@ if($database->connect_errno) {
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-    <link rel="stylesheet" href="css/jquery-ui.min.css">
+		<link rel="stylesheet" href="css/jquery-ui.min.css">
     <link rel="stylesheet" href="css/jquery-ui.theme.min.css">
 
 
@@ -73,35 +71,33 @@ if($database->connect_errno) {
   </div>
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.dataTables.min.js"></script>
-    <script src="js/dataTables.bootstrap.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
 </body>
 </html>
 
 <script type="text/javascript">
 
-    $.datepicker.regional['es'] = {
-    closeText: 'Cerrar',
-    prevText: '< Ant',
-    nextText: 'Sig >',
-    currentText: 'Hoy',
-    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-    monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
-    dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-    dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
-    dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
-    weekHeader: 'Sm',
-    dateFormat: 'dd/mm/yy',
-    firstDay: 1,
-    isRTL: false,
-    showMonthAfterYear: false,
-    yearSuffix: ''
-    };
-    $.datepicker.setDefaults($.datepicker.regional['es']);
-    $(function () {
-    $(".date").datepicker();
-    });
+$.datepicker.regional['es'] = {
+closeText: 'Cerrar',
+prevText: '< Ant',
+nextText: 'Sig >',
+currentText: 'Hoy',
+monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+weekHeader: 'Sm',
+dateFormat: 'dd/mm/yy',
+firstDay: 1,
+isRTL: false,
+showMonthAfterYear: false,
+yearSuffix: ''
+};
+$.datepicker.setDefaults($.datepicker.regional['es']);
+$(function () {
+$(".date").datepicker();
+});
 
     function addForm(e){
       e.preventDefault();
@@ -109,7 +105,7 @@ if($database->connect_errno) {
       var anio = fecha.getFullYear();
       var nuevoPrograma = parseInt(document.getElementById('programaActual').value)+1;
       var strForm = '<h2 align="center">Creación del nuevo programa</h2><br> ';
-      strForm = strForm + '<form method="post" id="main-form" class="form-horizontal" action="programCreate.php">';
+      strForm = strForm + '<fieldset><form method="post" id="main-form" class="form-horizontal" action="programCreate.php">';
       strForm = strForm + '<input type="hidden" id="programa" name="programa" value="'+nuevoPrograma+'">';
 
       strForm = strForm + '<div class="form-group">';
@@ -132,18 +128,41 @@ if($database->connect_errno) {
       strForm = strForm + '<h5 align="center">Fechas de realización del programa:</h5>';
 
       strForm = strForm + '<div class="form-group">';
-      strForm = strForm + '<label class="control-label col-sm-5" for="nombre">Nombre del programa:</label>';
-      strForm = strForm + '<div class="col-sm-3">';
-      strForm = strForm + '<input type="text" class="form-control" id="nombrePrograma" name="nombrePrograma" value="'+nuevoPrograma+' PROGRAMA DE CURSOS LIBRES" readonly="true">';
+      strForm = strForm + '<label class="control-label col-sm-3" for="fechaPro1">Inicio:</label>';
+      strForm = strForm + '<div class="col-sm-2">';
+      strForm = strForm + '<input type="text" class="form-control date" id="fechaPro1" name="fechaPro1" size="8" required>';
       strForm = strForm + '</div>';
-      strForm = strForm + '<label class="control-label col-sm-5" for="nombre">Nombre del programa:</label>';
-      strForm = strForm + '<div class="col-sm-3">';
-      strForm = strForm + '<input type="text" class="form-control" id="nombrePrograma" name="nombrePrograma" value="'+nuevoPrograma+' PROGRAMA DE CURSOS LIBRES" readonly="true">';
+
+      strForm = strForm + '<label class="control-label col-sm-2" for="fechaPro2">Fin:</label>';
+      strForm = strForm + '<div class="col-sm-2">';
+      strForm = strForm + '<input type="text" class="form-control date" id="fechaPro2" name="fechaPro2" size="8" required>';
       strForm = strForm + '</div>';
       strForm = strForm + '</div><br>';
 
-      strForm = strForm + '</form>';
+      strForm = strForm + '<h5 align="center">Fechas de inscripción:</h5>';
+
+      strForm = strForm + '<div class="form-group">';
+      strForm = strForm + '<label class="control-label col-sm-3" for="fechaIns1">Inicio:</label>';
+      strForm = strForm + '<div class="col-sm-2">';
+      strForm = strForm + '<input type="text" class="form-control date" id="fechaIns1" name="fechaIns1" size="8" required>';
+      strForm = strForm + '</div>';
+
+      strForm = strForm + '<label class="control-label col-sm-2" for="fechaIns2">Fin:</label>';
+      strForm = strForm + '<div class="col-sm-2">';
+      strForm = strForm + '<input type="text" class="form-control date" id="fechaIns2" name="fechaIns2" size="8" required>';
+      strForm = strForm + '</div>';
+      strForm = strForm + '</div><br>';
+
+      strForm = strForm + '<div class="form-group">';
+      strForm = strForm + '<div class="col-sm-offset-6 col-sm-10">';
+      strForm = strForm + '<input type="submit" class="btn btn-success" name="crearPrograma" value="Crear Programa">';
+      strForm = strForm + '</div>';
+      strForm = strForm + '</div><br>';
+
+      strForm = strForm + '</form></fieldset>';
       document.getElementById('formulario').innerHTML = strForm;
+
+      $(".date").datepicker();
     }
 
     $(document).ready(function() {
