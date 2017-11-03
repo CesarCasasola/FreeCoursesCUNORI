197e7b1e49fa7b942666731bce22b4a2c9b8722f
CECasasola <cecm2893@gmail.com>
2017-11-03 13:09:04 -0600

    terminada la generacion de listados

--- listGenerator.php
+++ listGenerator.php
@@ -1,6 +1,26 @@
+<!--<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/dataTables.bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container">-->
+
+
 <?php
 
-  $idCurso = 5;
+$idCurso = filter_input(INPUT_POST, "idCurso", FILTER_VALIDATE_INT);
+
+if ($idCurso) {
 
   $dbserver = '127.0.0.1';
   $dbuser = 'root';
@@ -22,60 +42,132 @@
     $area='';
     while($regCurso  = $cursInfo->fetch_array( MYSQLI_BOTH)){
       $nombreCurso = $regCurso['NOMBRE'];
-      $horario = $regCurso['HORAINICIO'].' - '.$regCurso['HORAFIN'];
+      $horario = substr($regCurso['HORAINICIO'], 0, 5).' - '.substr($regCurso['HORAFIN'], 0, 5);
       $dia = $regCurso['DIA'];
       $area = $regCurso['AREA'];
     }
-    $estudiantes = $database->query("SELECT E.NOMBRE, E.SEGUNDONOMBRE, E.APELLIDO, E.SEGUNDOAPELLIDO, E.DPI_ESTUDIANTE, E.TELEFONO, E.CORREO, E.FECHANAC, E.SEXO, B.CONFIRMADA FROM CURSO_BOLETA C INNER JOIN  BOLETA_ASIGNACION B ON C.CODIGO=B.CODIGO INNER JOIN ESTUDIANTE E ON B.DPI_ESTUDIANTE=E.DPI_ESTUDIANTE WHERE C.IDCURSO=$idCurso ORDER BY E.APELLIDO ASC");
+
+    $estudiantes = $database->query("SELECT E.NOMBRE, E.SEGUNDONOMBRE, E.APELLIDO, E.SEGUNDOAPELLIDO, E.DPI_ESTUDIANTE, E.TELEFONO, E.CORREO, TIMESTAMPDIFF(YEAR, E.FECHANAC, CURDATE()) AS EDAD, E.SEXO, B.CONFIRMADA FROM CURSO_BOLETA C INNER JOIN  BOLETA_ASIGNACION B ON C.CODIGO=B.CODIGO INNER JOIN ESTUDIANTE E ON B.DPI_ESTUDIANTE=E.DPI_ESTUDIANTE WHERE C.IDCURSO=$idCurso ORDER BY E.APELLIDO ASC");
+
+    $confirmados = $database->query("SELECT COUNT(E.NOMBRE) AS CONFIRMADOS FROM ESTUDIANTE E INNER JOIN BOLETA_ASIGNACION B ON E.DPI_ESTUDIANTE = B.DPI_ESTUDIANTE INNER JOIN CURSO_BOLETA C ON B.CODIGO = C.CODIGO WHERE C.IDCURSO =$idCurso AND B.CONFIRMADA=1");
+    $numConfirmados = 5;
+    while ($regConf = $confirmados->fetch_array( MYSQLI_BOTH )) {
+      $numConfirmados = $regConf['CONFIRMADOS'];
+    }
+
+    $noConfirmados = $database->query("SELECT COUNT(E.NOMBRE) AS NOT_CONFIRMED FROM ESTUDIANTE E INNER JOIN BOLETA_ASIGNACION B ON E.DPI_ESTUDIANTE = B.DPI_ESTUDIANTE INNER JOIN CURSO_BOLETA C ON B.CODIGO = C.CODIGO WHERE C.IDCURSO =$idCurso AND B.CONFIRMADA=0");
+    $numNoConfirmados = 8;
+    while ($regNoConf = $noConfirmados->fetch_array( MYSQLI_BOTH )) {
+      $numNoConfirmados = $regNoConf['NOT_CONFIRMED'];
+    }
+
+
+    $str = '<div class="well well-sm">
+      <p><b>Curso: </b>'. $nombreCurso.' <br>
+        <b>Área: </b>  '.$area.' <br>
+        <b>Día: </b>'.$dia.'<br>
+        <b>Horario: </b>'.$horario.'<br>  <b>';
+
+        if (mysqli_num_rows($docentes)>1) {
+          $str = $str. 'Docentes: ';
+        }else {
+          $str = $str. 'Docente: ';
+        }
+         $str = $str.'</b>';
+
+       while ($regDoc = $docentes->fetch_array( MYSQLI_BOTH )) {
+               $str = $str.'<div style="text-indent: 40pt;">';
+               $str = $str.$regDoc['NOMBRE'].' '.$regDoc['APELLIDO'];
+               $str = $str. '</div>';
+       }
+      $str = $str.'</p> </div> ';
+
+      $str = $str.'<div>
+                <form method="post" id="main-form" action="">
+                    <input type="hidden" name="idCurso" value="'.$idCurso.'">
+
+                    <div class="form-group">
+                      <div class="col-sm-4">
+                        <button type="submit" name="listaInscripcion" class="btn btn-primary center-block">
+                          <span class="glyphicon glyphicon-th-list"></span>  Imprimir listado de Inscripción
+                        </button>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <div class="col-sm-4">
+                        <button type="submit" name="listaAsistencia" class="btn btn-success center-block">
+                          <span class="glyphicon glyphicon-ok"></span>  Imprimir lista de Asistencia
+                        </button>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <div class="col-sm-4">
+                        <button type="submit" name="cartaCompromiso" class="btn btn-info center-block">
+                          <span class="glyphicon glyphicon-file"></span>  Imprimir Cartas de Compromiso
+                        </button>
+                      </div>
+                    </div>
+
+                  </form> </div><br><br><br><br>';
+
+      $str = $str.'<div>
+            <h3 align="center">Estudiantes inscritos</h3>
+            <ul class="list-group">
+              <li class="list-group-item list-group-item-success">Confirmados. <span class="badge">'.$numConfirmados.'</span></li>
+              <li class="list-group-item">No Confirmados. <span class="badge">'.$numNoConfirmados.'</span></li>
+            </ul>
+
+      			<table class="table table-active table-responsive table-bordered table-condensed table-striped" id="miTabla">
+
+      					<thead>
+      					<tr class="bg-primary">
+                  <th>No.</th>
+                  <th>Nombre</th>
+                  <th>Teléfono</th>
+                  <th>Correo</th>
+                  <th>DPI</th>
+                  <th>Edad</th>
+                  <th>Sexo</th>
+                </tr>
+                </thead>
+                <tbody>';
+
+      $i = 1;
+      while ($regEst = $estudiantes->fetch_array( MYSQLI_BOTH )) {
+
+          if($regEst['CONFIRMADA']){
+            $col = 'bg-success txt-success';
+          }else {
+            $col = 'txt-muted';
+          }
+
+          $str = $str.'<tr class="'.$color.'">
+            <th>'.$i.'</th>
+            <th>'.$regEst['NOMBRE'].' '.$regEst['SEGUNDONOMBRE'].' '.$regEst['APELLIDO'].' '.$regEst['SEGUNDOAPELLIDO'].'</th>
+            <th>'.$regEst['TELEFONO'].'</th>
+            <th>'.$regEst['CORREO'].'</th>
+            <th>'.$regEst['DPI_ESTUDIANTE'].'</th>
+            <th>'.$regEst['EDAD'].'</th>
+            <th>'.$regEst['SEXO'].'</th>
+          </tr>';
+
+          $i++;
+      }
+
+      $str = $str.'</tbody> </table> </div>';
+      echo $str;
 
   }
-
+}
  ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-		<!-- Optional theme -->
 
-		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.dataTables.min.js"></script>
-    <script src="js/dataTables.bootstrap.min.js"></script>
-  </head>
-  <body>
-    <div class="container">
-      <div class="well well-sm">
-        <p><b>Curso: </b> <?php echo $nombreCurso; ?><br>
-          <b>Área: </b> <?php echo $area; ?> <br>
-          <b>Horario </b> <?php echo $horario; ?><br>
-          <b>Día:</b> <?php echo $dia; ?><br>
-          <b><?php
-          if (mysqli_num_rows($docentes)>1) {
-            echo 'Docentes: ';
-          }else {
-            echo 'Docente: ';
-          }
-           ?></b><br>
 
-             <?php
-             while ($regDoc = $docentes->fetch_array( MYSQLI_BOTH )) {
-                 echo '<div style="text-indent: 40pt;">';
-                 echo $regDoc['NOMBRE'].' '.$regDoc['APELLIDO'];
-                 echo '</div>';
-             }
-              ?>
-        </p>
-      </div>
 
-      
-
-    </div>
+  <!--  </div>
 
 
   </body>
-</html>
+</html>-->
---
+++ listados.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+    <link href="css/select2.min.css" rel="stylesheet"/>
+</head>
+
+<body>
+
+  <?php
+    $dbserver = '127.0.0.1';
+    $dbuser = 'root';
+    $password = 'dbn0w';
+    $dbname = 'cursos_libres';
+
+    $database = new mysqli($dbserver, $dbuser, $password, $dbname);
+
+    if($database->connect_errno) {
+      die("No se pudo conectar a la base de datos");
+    }else{
+      $cursOptions='<option value=""></option>';
+      $cursos="SELECT C.IDCURSO, C.NOMBRE FROM CURSO C INNER JOIN PROGRAMA P ON C.IDPROGRAMA=P.IDPROGRAMA WHERE P.ESTADO=1 order by NOMBRE ASC";
+      $queryCursos= $database->query($cursos);
+
+      while($registroCurso  = $queryCursos->fetch_array( MYSQLI_BOTH)){
+        $cursOptions = $cursOptions.' <option value="'.$registroCurso['IDCURSO'].'">'.$registroCurso['NOMBRE'].'</option>';
+      }
+    }
+   ?>
+
+
+<div class="container">
+<div>
+  <h1 align="center">Generación de Listados</h1>
+</div><br><br>
+
+<div class="form-group">
+  <label class="control-label col-sm-1 col-sm-offset-2" for="area">Curso:</label>
+  <div class="col-sm-8">
+    <select class="form-control pick" id="curso" name="curso">
+      <?php echo $cursOptions; ?>
+    </select>
+  </div>
+</div><br><br>
+
+<div id="contenido">
+
+</div>
+
+</div>
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap.min.js"></script>
+<script src="js/select2.min.js"></script>
+
+
+</body>
+</html>
+
+
+  <script type="text/javascript">
+
+    function addContent(){
+      var curso = document.getElementById("curso").value;
+      var parametros = {
+        "idCurso" : curso
+      };
+
+      $.ajax({
+        		data:	parametros,
+        		url:	'listGenerator.php',
+        		type: 	'post',
+        		success: 	function(data){
+				    $('#contenido').html(data);
+				    },
+			      error : 	function() {
+			           console.log('error');
+			      }
+        	});
+
+        $('#miTabla').dataTable();
+
+    }
+
+    $(document).ready(function(){
+      $('#curso').change(addContent);
+      $('#miTabla').dataTable();
+      $('.pick').select2();
+    });
+
+  </script>
